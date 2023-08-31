de32d5ca9fcea7b019b538a051ea31702874a6ec
Dimitri Schachmann <d.schachmann@gmail.com>
2023-08-31 16:27:14 +0200

    stream data in example to trigger error

--- examples/next-openai/app/api/chat/route.ts
+++ examples/next-openai/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 // ./app/api/chat/route.ts
 import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAIStream, StreamingTextResponse, experimental_StreamData } from 'ai'
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
@@ -14,6 +14,8 @@ export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json()
 
+  const data = new experimental_StreamData()
+
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
@@ -22,7 +24,15 @@ export async function POST(req: Request) {
   })
 
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
+  const stream = OpenAIStream(response, {experimental_streamData: true})
+
+  for(let i = 0; i < 1000; i++) {
+    data.append({
+      text: 'Hello, how are you?',
+    })
+  }
+  data.close()
+
   // Respond with the stream
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream, {}, data)
 }
